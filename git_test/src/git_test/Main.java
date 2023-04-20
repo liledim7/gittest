c73c513a26d02ef0cf02f2d470ebc7c2921d8583
Dongje Lee <mgh0222@naver.com>
2023-04-20 17:21:10 +0900

    Git Test

---
+++ git_test/src/git_test/Main.java
@@ -0,0 +1,14 @@
+package git_test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * 1. import를 통해 git_test 가져오기
+		 * 2. 각자 자기 이름으로 '@@@ 성공!' 프린트하기
+		 * 3. push해서 업데이트
+		 */
+		System.out.println("이동제 성공!");
+	}
+
+}
