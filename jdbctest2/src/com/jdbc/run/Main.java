fe47239008ebe151a15f71851cfa3d8903af3c46
GD <GD@DESKTOP-UTVUR1O>
2023-04-20 15:09:57 +0900

    03 sw

--- jdbctest2/src/com/jdbc/run/Main.java
+++ jdbctest2/src/com/jdbc/run/Main.java
@@ -5,6 +5,7 @@ import com.jdbc.controller.MemberControllerImpl;
 public class Main {
 	public static void main(String[] args) {
 		new MemberControllerImpl().mainMenu();
-		System.out.println("내가 추가한것");
+		int age=10;
+		System.out.println(age);
 	}
 }
